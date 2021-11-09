c96709d023cac404d3be05268cca585a7a4d6f17
tsuchida-yuki1 <tsuchida-yuki1@dmm.com>
2021-11-10 04:26:54 +0900

    MIN_ESTIMATE_HISTORY_LEN=5

--- cmd/experiment/est_greedy_speed_done.go
+++ cmd/experiment/est_greedy_speed_done.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	DEBUG                    = true
-	MIN_ESTIMATE_HISTORY_LEN = 0   //良さそうなのは30
+	MIN_ESTIMATE_HISTORY_LEN = 5   //良さそうなのは30
 	HC_LOOP_COUNT            = 200 //増やせばスコアは伸びるか？
 	FREE_MARGIN              = 4
 )
