2dd2ea5d695e5f60a2be93708f42278dc06d04c5
tsuchida-yuki1 <tsuchida-yuki1@dmm.com>
2021-11-11 12:30:51 +0900

    下振れ補正, MARGIN=3

--- cmd/experiment/est_greedy_speed2.go
+++ cmd/experiment/est_greedy_speed2.go
@@ -14,7 +14,7 @@ const (
 	DEBUG                    = true
 	MIN_ESTIMATE_HISTORY_LEN = 0  //良さそうなのは30
 	HC_LOOP_COUNT            = 50 //増やせばスコアは伸びるか？
-	FREE_MARGIN              = 4  //
+	FREE_MARGIN              = 3  //
 )
 
 var (
